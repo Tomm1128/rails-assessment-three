f1512781ab11d8ef6935d888d3795db450893a7f
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-21 09:25:39 -0400

    Create migrations and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,30 @@
 #
 #   1
 Pizzeria.destroy_all
- 
-Pizzeria.create!([{
-  name: "Sottocasa NYC",
-  address: "298 Atlantic Ave, Brooklyn, NY 11201",
-},
-{
-  name: "PizzArte",
-  address: "69 W 55th St, New York, NY 10019",
-},
-{
-  name: "San Matteo NYC",
-  address: "1559 2nd Ave, New York, NY 10028"
-}])
+Pizza.destroy_all
+
+pizzerias = Pizzeria.create!([
+  {
+    name: "Sottocasa NYC",
+    address: "298 Atlantic Ave, Brooklyn, NY 11201",
+  },
+  {
+    name: "PizzArte",
+    address: "69 W 55th St, New York, NY 10019",
+  },
+  {
+    name: "San Matteo NYC",
+    address: "1559 2nd Ave, New York, NY 10028"
+  }
+])
+
+pizzas = [
+  { name: "Margherita", description: "Tomato, mozzarella, and basil", pizzeria: pizzerias[0] },
+  { name: "Marinara", description: "Tomato, garlic, oregano, and olive oil", pizzeria: pizzerias[0] },
+  { name: "Quattro Stagioni", description: "Tomatoes, artichokes, ham, mushrooms, and olives", pizzeria: pizzerias[1] },
+  { name: "Diavola", description: "Tomato, mozzarella, and spicy salami", pizzeria: pizzerias[1] },
+  { name: "Capricciosa", description: "Tomato, mozzarella, artichokes, ham, mushrooms, and olives", pizzeria: pizzerias[2] },
+  { name: "Napoli", description: "Tomato, mozzarella, anchovies, and capers", pizzeria: pizzerias[2] }
+]
+
+Pizza.create!(pizzas)
